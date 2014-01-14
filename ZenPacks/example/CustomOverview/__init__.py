0e528ae45d1b7241ded7ed112f643c30279fe45f
samueljon <sammi@villingaholt.nu>
2014-01-14 17:52:46 -0500

    Added contract field to the system id summary

--- ZenPacks/example/CustomOverview/__init__.py
+++ ZenPacks/example/CustomOverview/__init__.py
@@ -5,6 +5,8 @@ from Products.Zuul.infos.device import DeviceInfo
 
 # Set a default value for a device's contact.
 Device.contact = ''
+Device.contract = ''
 
 # Make a device's contact available through the API.
 DeviceInfo.contact = ProxyProperty('contact')
+DeviceInfo.contact = ProxyProperty('contract')
--- ZenPacks/example/CustomOverview/resources/custom-overview-device.js
+++ ZenPacks/example/CustomOverview/resources/custom-overview-device.js
@@ -1,18 +1,29 @@
+//Little more details about the overviewpanel http://wiki.zenoss.org/Device_Overview_Panels
 (function(){
 
-Ext.onReady(function() {
+ Ext.onReady(function() {
 
-    // Add information to the device overview page.
-    var PANEL_ID = 'deviceoverviewpanel_systemsummary';
-    Ext.ComponentMgr.onAvailable(PANEL_ID, function(){
-        var panel = Ext.getCmp(PANEL_ID);
-
-        panel.insert(0, {
-            name: 'contact',
-            xtype: 'textfield',
-            fieldLabel: _t('Technical Contact')
-        });
-    });
+	 // Add information to the device overview page.
+	 var PANEL_ID = 'deviceoverviewpanel_systemsummary';
+	 Ext.ComponentMgr.onAvailable(PANEL_ID, function(){
+		 var panel = Ext.getCmp(PANEL_ID);
+		 //INDEX0
+		 panel.insert(0, {
+			name: 'contact',
+			xtype: 'textfield',
+			fieldLabel: _t('Technical Contact')
+		});
+	});
+	var PANEL_ID2 = 'deviceoverviewpanel_idsummary';
+	Ext.ComponentMgr.onAvailable(PANEL_ID2, function(){
+		 var panel = Ext.getCmp(PANEL_ID2);
+		 //INDEX0
+		 panel.insert(5, {
+			name: 'contract',
+			xtype: 'textfield',
+			fieldLabel: _t('Contract')
+		});
+	});
 });
 
 })();
